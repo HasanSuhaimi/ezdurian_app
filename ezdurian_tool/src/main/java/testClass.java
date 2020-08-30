b2a48947402a1d76f605c94f3b5563bba9734513
Muhammad Hasan Mohd Suhaimi <000389@tngdigital.com.my>
2020-08-30 19:01:13 +0800

    update with sending whatsapp text functionality

--- ezdurian_tool/src/main/java/testClass.java
+++ ezdurian_tool/src/main/java/testClass.java
@@ -6,7 +6,9 @@ import com.gargoylesoftware.htmlunit.html.*;
 import com.gargoylesoftware.htmlunit.WebClient;
 
 import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 //webdriver class
 import org.openqa.selenium.By;
@@ -23,8 +25,8 @@ public class testClass {
         System.out.println("test");
 
         testClass test = new testClass();
-        test.SeleniumPulldata();
-        //test.Seleniumtest();
+        //test.SeleniumPulldata();
+        test.SeleniumSendMsg();
         // create webclient
         /*final WebClient webClient = new WebClient(BrowserVersion.CHROME);
         webClient.getOptions().setUseInsecureSSL(true); //ignore ssl certificate
@@ -154,11 +156,39 @@ public class testClass {
 
     }
 
-    public void Seleniumtest() throws Exception {
+    public void SeleniumSendMsg() throws Exception {
 
         ChromeDriver driver = new ChromeDriver();
         driver.get("https://web.whatsapp.com/");
-        Thread.sleep(50000);  // Let the user actually see something!
+        Thread.sleep(7000);  // Let the user actually see something!
+        //get search button
+        WebElement boxSearch = driver.findElement(By.xpath("//button[@class=\"_3e4VU\"]"));
+        //click search button
+        boxSearch.click();
+        //search contact
+        String name = "KC Sheffield" ;
+        WebElement searchContact = driver.findElement(By.xpath("//div[@class=\"_3FRCZ copyable-text selectable-text\"]"));
+        searchContact.sendKeys(name);
+        //open chat with the contact
+        WebElement targetContact = driver.findElement(By.xpath("//span[@title="+ "\"" + name + "\"" +"]"));
+        targetContact.click();
+
+        //get textBox and send message
+        WebElement textBox = driver.findElement(By.xpath("//div[@class=\"_3FRCZ copyable-text selectable-text\"][@contenteditable=\"true\"][@data-tab=\"1\"]"));
+
+        for(int i = 0; i <=20 ; i++){
+        textBox.sendKeys("Testing");
+        textBox.sendKeys(Keys.ENTER);
+        }
+        textBox.sendKeys("Settle");
+        textBox.sendKeys(Keys.ENTER);
+        Thread.sleep(5000);
+        //boxSearch.click();
+        //Thread.sleep(100);
+        //boxSearch.sendKeys("KC");
+
+        //System.out.println(boxSearch.isDisplayed());
+
         driver.quit();
     }
 
@@ -198,6 +228,7 @@ public class testClass {
 
         reportButton.sendKeys(Keys.ENTER);
 
+        // get the whole table
         WebElement reportTable = driver.findElement(By.xpath("//table/tbody"));
 
         // make a list of all rows (/tr) in the table
@@ -205,25 +236,82 @@ public class testClass {
 
         int table_size = rows_table.size();
 
+        // initiate a list of data
+        List <data> datas = new ArrayList<data>();
+
         for(int x =0 ; x < table_size; x++)
         {
             //for each row, get all the columns
             List <WebElement> column_row = rows_table.get(x).findElements(By.tagName("td"));
 
-            //if No.Invois for NP store the info
+            //if No.Invois for NP, initiate data class, store the info and add into the list of data (datas)
             if(column_row.get(1).getText().contains("NP"))
             {
-                System.out.println(rows_table.get(x).getText() + "\n");
+                data Data = new data();
+                //System.out.println(rows_table.get(x).getText() + "\n");
+                Data.setInvoice(column_row.get(1).getText());
+                Data.setName(column_row.get(2).getText());
+                //skip email
+                Data.setNumber(column_row.get(4).getText());
+                Data.setAdress(column_row.get(5).getText());
+                Data.setField1(column_row.get(6).getText());
+                Data.setField2(column_row.get(7).getText());
+                Data.setProduct(column_row.get(8).getText());
+                Data.setConfirmed_at(column_row.get(9).getText());
+                Data.setStatus(column_row.get(10).getText());
+
+                datas.add(Data);
             }
 
         }
 
-        Thread.sleep(5000);
+        System.out.println(datas.get(1).getAdress());
 
-        //driver.quit();
+        //Thread.sleep(5000);
+
+        driver.quit();
 
     }
 
+    public static class data {
 
+        private String invoice;
+        private String name;
+        private String number;
+        private String adress;
+        private String field1;
+        private String field2;
+        private String product;
+        private String confirmed_at;
+        private String status;
+
+        public void setInvoice (String value) {this.invoice = value;}
+        public String getInvoice () { return invoice; }
+
+        public void setName (String value) {this.name = value;}
+        public String getName () { return name; }
+
+        public void setNumber (String value) {this.number = value;}
+        public String getNumber () { return number; }
+
+        public void setAdress (String value) {this.adress = value;}
+        public String getAdress () { return adress; }
+
+        public void setField1 (String value) {this.field1 = value;}
+        public String getField1 () { return field1; }
+
+        public void setField2 (String value) {this.field2 = value;}
+        public String getField2 () { return field2; }
+
+        public void setProduct (String value) {this.product = value;}
+        public String getProduct () { return product; }
+
+        public void setConfirmed_at (String value) {this.confirmed_at = value;}
+        public String getConfirmed_at () { return confirmed_at; }
+
+        public void setStatus (String value) {this.status = value;}
+        public String getStatus () { return status; }
+
+    }
 }
 
